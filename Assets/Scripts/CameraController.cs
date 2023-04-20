3d62873d902227ca0ebceeb425053d1dc221d5f3
James <61620217+vezinjames@users.noreply.github.com>
2023-04-20 16:24:59 +0200

    Map change

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float damping;
+
+    private Vector3 velocity;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movePosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+    }
+}
